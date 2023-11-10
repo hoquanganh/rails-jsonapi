cc3627f7b552602404002909150294f0f63602e1
Quang Anh Ho <quanganh.ho7@gmail.com>
2023-11-10 21:47:36 +0700

    add more API errors handle

--- app/controllers/api/exception_rescue.rb
+++ app/controllers/api/exception_rescue.rb
@@ -16,6 +16,21 @@ module Api
       def render_record_not_found_response exception
         render json: Api::Errors::ActiveRecordNotFound.new(exception).to_hash, status: :not_found
       end
+
+      # Other errors handle
+      rescue_from(Unauthorized) do |exception|
+        render json: { errors: "Unauthorized" }, status: 401
+      end
+      rescue_from(UnprocessableEntity) do |exception|
+        render json: { errors: "Unprocessable Entity" }, status: 422
+      end
+      rescue_from(BadRequest) do |exception|
+        render json: { errors: "There's something wrong with your request" }, status: 400
+      end
+
+      rescue_from(Forbidden) do |_exception|
+        render json: { errors: "You are not allowed" }, status: 403
+      end
     end
   end
 end
